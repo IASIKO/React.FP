fecc630ea93373567a55c244d5f8f1933c769635
IASIKO <103432601+IASIKO@users.noreply.github.com>
2023-01-29 12:36:01 +0400

    add

--- src/components/product/singleProductPage/index.js
+++ src/components/product/singleProductPage/index.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
-import { fetchSingleProductById, useSingleProduct } from "../../../redux";
+import {
+  fetchSingleProductById,
+  useSingleProduct,
+} from "../../../redux";
+import { Box, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+
+const StyleTypography = styled(Typography)(() => ({
+  margin: "20px",
+  fontSize: "20px",
+}));
 
 export const DetailedProduct = () => {
   const { state } = useLocation();
@@ -12,5 +22,16 @@ export const DetailedProduct = () => {
   useEffect(() => {
     dispatch(fetchSingleProductById({ id: state.id, category: categoryName }));
   }, [state.id]);
-  return <div>DetailedProductPage</div>;
+  return (
+    <Box>
+      <img src={singleProduct?.image} />
+      <Box>
+        <StyleTypography> Name : {singleProduct?.name} </StyleTypography>
+        <StyleTypography> Price : $ {singleProduct?.price} </StyleTypography>
+        <StyleTypography> Brand : {singleProduct?.brand} </StyleTypography>
+        <StyleTypography> Category : {singleProduct?.category} </StyleTypography>
+        <StyleTypography> Description : {singleProduct?.description} </StyleTypography>
+      </Box>
+    </Box>
+  )
 };
